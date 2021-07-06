93512e18a0b29435a5910d9bc777276aa481d1a1
rahulsinghbisht-13 <rbisht7332gmail.com>
2021-07-06 16:48:24 +0530

    Cleaned code and added folder structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
