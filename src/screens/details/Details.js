e3d1f9070f4b5a13a45f188d469ab431117543f0
rahulsinghbisht-13 <rbisht7332gmail.com>
2021-07-09 15:40:05 +0530

    Added artists gridlist on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -6,6 +6,9 @@ import Typography from '@material-ui/core/Typography';
 import Home from '../home/Home';
 import ReactDOM from 'react-dom';
 import YouTube from 'react-youtube';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 class Details extends Component {
 
@@ -27,6 +30,10 @@ class Details extends Component {
 
     backToHomeHandler = () => {
         ReactDOM.render(<Home />, document.getElementById('root'));
+    }
+
+    artistClickHandler = (url) => {
+        window.location = url;
     }
 
     render() {
@@ -89,7 +96,26 @@ class Details extends Component {
                         </div>
                     </div>
                     <div className="rightDetails">
-
+                        <div className="bold marginBottom16 marginTop16">
+                            <Typography>
+                                <span className="bold">Artists:</span>
+                            </Typography>
+                        </div>
+                        <div className="paddingRight">
+                            <GridList cellHeight={160} cols={2}>
+                                {movie.artists != null && movie.artists.map(artist => (
+                                    <GridListTile
+                                        className="gridTile"
+                                        onClick={() => this.artistClickHandler(artist.wiki_url)}
+                                        key={artist.id}>
+                                        <img src={artist.profile_url} alt={artist.first_name + " " + artist.last_name} />
+                                        <GridListTileBar
+                                            title={artist.first_name + " " + artist.last_name}
+                                        />
+                                    </GridListTile>
+                                ))}
+                            </GridList>
+                        </div>
                     </div>
                 </div>
             </div>
