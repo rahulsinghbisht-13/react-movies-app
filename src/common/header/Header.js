c9190c286dcfd55e92d2853bfdac3cccade7cba0
rahulsinghbisht-13 <rbisht7332gmail.com>
2021-07-10 13:22:26 +0530

    Integrated logout functionality

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -181,7 +181,12 @@ class Header extends Component {
     }
 
     logoutHandler = (e) => {
+        sessionStorage.removeItem("uuid");
+        sessionStorage.removeItem("access-token");
 
+        this.setState({
+            loggedIn: false
+        });
     }
 
     render() {
